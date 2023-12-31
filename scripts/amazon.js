c669ee03b87f326ea08f137b44352a0299f399d4
Priyanshu Mrinal <priyanshu_jha.ug22@nsut.ac.in>
2023-12-31 19:10:01 +0530

    use product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,69 @@
 // Data Structure for list of products
-const products=[{
-    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-        stars:4.5,
-        count:87
-    },
-    priceCents:1090
-},{
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name:'Intermediate Size Basketball',
-    rating:{
-        stars:4,
-        count:127
-    },
-    priceCents:2095
-},{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        stars:4.5,
-        count:56
-    },
-    priceCents:799
-}];
+// const products=[{
+//     image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating:{
+//         stars:4.5,
+//         count:87
+//     },
+//     priceCents:1090
+// },{
+//     image:'images/products/intermediate-composite-basketball.jpg',
+//     name:'Intermediate Size Basketball',
+//     rating:{
+//         stars:4,
+//         count:127
+//     },
+//     priceCents:2095
+// },{
+//     image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name:'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating:{
+//         stars:4.5,
+//         count:56
+//     },
+//     priceCents:799
+// },{
+//     image:'images/products/knit-athletic-sneakers-gray.jpg',
+//     name:'Waterproof Knit Athletic Sneakers - Gray',
+//     rating:{
+//         stars:4,
+//         count:89
+//     },
+//     priceCents:3390
+// },{
+//     image:'images/products/cotton-bath-towels-teal.webp',
+//     name:'100% Cotton Bath Towels - 2 Pack, Light Teal',
+//     rating:{
+//         stars:4.5,
+//         count:93
+//     },
+//     priceCents:2110
+// },{
+//     image:'images/products/men-navigator-sunglasses-brown.jpg',
+//     name:'Men\'s Navigator Sunglasses Pilot',
+//     rating:{
+//         stars:3.5,
+//         count:42
+//     },
+//     priceCents:1690
+// },{
+//     image:'images/products/knit-athletic-sneakers-pink.webp',
+//     name:'Waterproof Knit Athletic Sneakers - Pink',
+//     rating:{
+//         stars:4,
+//         count:89
+//     },
+//     priceCents:3390
+// },{
+//     image:'images/products/men-cozy-fleece-zip-up-hoodie-red.jpg',
+//     name:'Men\'s Full-Zip Hooded Fleece Sweater',
+//     rating:{
+//         stars:4.5,
+//         count:3157
+//     },
+//     priceCents:2400
+// }];
 
 let productHTML='';
 products.forEach((product)=>{
